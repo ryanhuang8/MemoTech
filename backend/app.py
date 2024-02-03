0c8a526baefa8eb7b55c6e858b976efc622ee212
Eric <dasomi04@gmail.com>
2024-02-03 14:08:46 -0500

    feat: connected MongoDb w/ Flask

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from bson import json_util
 
 app = Flask(__name__)
 
+# Configure MongoDB connection
+app.config['MONGO_URI'] = 'mongodb+srv://dasomi04:Ys78O453Etbhe7IZ@cluster1.29ruiho.mongodb.net/?retryWrites=true&w=majority'
+mongo = MongoClient(app.config['MONGO_URI'])
+db = mongo.get_database('MemoTech')
+collection = db['sat words']
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    data_from_mongo = collection.find_one({})
+    json_data = json_util.dumps(data_from_mongo)
+    return jsonify(message='Hello, MongoDB with Flask!', data_from_mongo=json_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
