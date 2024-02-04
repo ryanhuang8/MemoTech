ef38c99b8ee987c89970c203ab05b0a0dc8d2276
huangr0867 <rhuang53@gmail.com>
2024-02-04 04:48:25 -0500

    feat: style

--- frontend/src/pages/StartPage.tsx
+++ frontend/src/pages/StartPage.tsx
@@ -4,11 +4,11 @@ function StartPage() {
   return (
     <div>
         <Navbar />
-        <h1>MemoTech</h1>
+        <h1>Learn your flashcards with feedback</h1>
         <div id="stars1"></div>
         <div id="stars2"></div>
         {/* <div id="stars3"></div> */}
-        <li><a href="/import">Create Your Flashcards</a></li>
+        <a href="/import">Create Your Flashcards</a>
     </div>
     
   )
