67c990c99936b77ac078e26c28a6a4372b86168f
huangr0867 <rhuang53@gmail.com>
2024-02-04 06:28:28 -0500

    feat: added vector-search

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,19 @@
 from flask import Flask, jsonify, redirect, render_template, request, url_for
 import pymongo
 from bson import json_util
+from vector_search import vector_search
 # import json
 
-# import os
+import os
 
-# import openai
+import openai
 
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-# openai.api_key = "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"
-# os.environ["OPEN_API_KEY"] = "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"
+openai.api_key = "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"
+os.environ["OPEN_API_KEY"] = "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"
 # os.environ["ATLAS_CONNECTION_STRING"] = "mongodb+srv://ryanyhuang:ctwj2kRNIVNOV1Wd@cluster0.yualorr.mongodb.net/?retryWrites=true&w=majority"
 # client = pymongo.MongoClient(os.environ["ATLAS_CONNECTION_STRING"])
 # db = client.Memotech
@@ -21,8 +22,8 @@ CORS(app)
 # Configure MongoDB connection
 app.config['MONGO_URI'] = 'mongodb+srv://dasomi04:Ys78O453Etbhe7IZ@cluster1.29ruiho.mongodb.net/?retryWrites=true&w=majority'
 mongo = pymongo.MongoClient(app.config['MONGO_URI'])
-db = mongo.get_database('MemoTech')
-collection = db['main']
+db = mongo.get_database('Database')
+collection = db['vector-search']
 
 @app.route('/', methods=['GET'])
 def home():
@@ -46,23 +47,28 @@ def post_data():
 
     return jsonify(processed_data)
 
-# @app.route("/get-feedback")
-# def index():
-#     question = request.args.get('q')
-#     answer = request.args.get('a')
-#     completion = openai.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages=[
-#             {"role": "system", "content": "You are a grader, giving a student feedback for their answer to their question"},
-#             {"role": "user", "content": f"The question is '{question}' and they responded with '{answer}'. In your response, first say whether the student's answer is correct (either Yes or No). Then, give them feedback to help guide them to the correct answer without saying the answer"}
-#         ]
-#     )
+@app.route("/get-feedback")
+def index():
+    question = request.args.get('q')
+    answer = request.args.get('a')
+    completion = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a grader, giving a student feedback for their answer to their question"},
+            {"role": "user", "content": f"The question is '{question}' and they responded with '{answer}'. In your response, first say whether the student's answer is correct (either Yes or No). Then, give them feedback to help guide them to the correct answer without saying the answer"}
+        ]
+    )
 
-#     completion_message = completion.choices[0].message.content
-#     print(completion_message)
-#     response = jsonify({"message": completion_message})
+    completion_message = completion.choices[0].message.content
 
-#     return response
+    response = jsonify({"message": completion_message})
+
+    return response
+
+@app.route("/vector-search")
+def vec_search():
+    query = request.args.get('query')
+    return vector_search(query)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- backend/vector_search.py
+++ backend/vector_search.py
@@ -1,9 +1,9 @@
 import pymongo
 import requests
 
-client = pymongo.MongoClient("mongodb+srv://ryanyhuang:ctwj2kRNIVNOV1Wd@cluster0.yualorr.mongodb.net/?retryWrites=true&w=majority")
-db = client.Memotech
-collection = db.embeddings
+client = pymongo.MongoClient("mongodb+srv://dasomi04:Ys78O453Etbhe7IZ@cluster1.29ruiho.mongodb.net/?retryWrites=true&w=majority")
+db = client.Database
+collection = db.vector_search
 
 hf_token = "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"
 embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
@@ -20,9 +20,9 @@ def generate_embedding(text: str) -> list[float]:
 
   return response.json()
 
-# for doc in collection.find({'question':{"$exists":True}}):
-#    doc['plot_embedding_hf'] = generate_embedding(doc['question'])
-#    collection.replace_one({'_id': doc['_id']}, doc)
+for doc in collection.find({'premise':{"$exists":True}}):
+   doc['plot_embedding_hf'] = generate_embedding(doc['premise'])
+   collection.replace_one({'_id': doc['_id']}, doc)
 
 def vector_search(query):
 
@@ -30,14 +30,17 @@ def vector_search(query):
     {"$vectorSearch": {
       "queryVector": generate_embedding(query),
       "path": "plot_embedding_hf",
-      "numCandidates": 100,
-      "limit": 4, # num results returned
-      "index": "PlotSemanticSearch",
+      "numCandidates": 10,
+      "limit": 3, # num results returned
+      "index": "default",
       }}
   ])
-
+  
   q_and_a = {}
+  i = 0
   for document in results:
-      q_and_a[document["question"]] = document["answer"]
+      parsed = document["q_a"]
+      q_and_a[i] = parsed
+      i+=1
     
   return q_and_a
