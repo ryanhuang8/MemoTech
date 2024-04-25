8c6c49ba297cc75d4373d22324e10f4d8495869f
Bailey Smoko <bailey_smoko@brown.edu>
2024-04-25 16:23:43 -0400

    added light mode for start page

--- frontend/src/pages/StartPage.tsx
+++ frontend/src/pages/StartPage.tsx
@@ -4,34 +4,30 @@ import "../styles/StartPage.css";
 function StartPage() {
   return (
     <div className='start-page'>
-        <Navbar />
-
-        <div id="stars1"></div>
-        <div id="stars2"></div>
-        
-        <div id="solar-syst">
-            <div className="planet" id="sun"></div>
-            <div className="planet" id="mercury"></div>
-            <div className="planet" id="venus"></div>
-            <div className="planet" id="earth"></div>
-            <div className="planet" id="mars"></div>
-            {/* <div className="planet" id="jupiter"></div> */}
-            <div className="planet" id="saturn"></div>
-            <div className="planet" id="uranus"></div>
-            <div className="planet" id="neptune"></div>
-            <div className="planet" id="pluto"></div>
-            <div className="asteroid-belt"></div>
-        </div>
-
-        <h1>Memorize FlashCards with Feedback</h1>
-        <br></br>
-        <br></br>
-        <br></br>
-        <a className="a-button" href="/import">Create Your Flashcards</a>
-        
-
+      <Navbar />
+      <div id="stars1"></div>
+      <div id="stars2"></div>
+      <div id="solar-syst">
+        <div className="planet" id="sun"></div>
+        <div className="planet" id="mercury"></div>
+        <div className="planet" id="venus"></div>
+        <div className="planet" id="earth"></div>
+        <div className="planet" id="mars"></div>
+        {/* <div className="planet" id="jupiter"></div> */}
+        <div className="planet" id="saturn"></div>
+        <div className="planet" id="uranus"></div>
+        <div className="planet" id="neptune"></div>
+        <div className="planet" id="pluto"></div>
+        <div className="asteroid-belt"></div>
+      </div>
+      <h1>Memorize FlashCards with Feedback</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <a className="a-button" href="/create-flashcards">
+        <span className="button-text light-mode">Create Your Flashcards</span>
+      </a>
     </div>
-    
   )
 }
 
